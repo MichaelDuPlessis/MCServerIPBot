b5cd054fefb07cf25e98b366ac778a801a05ec80
Michael du Plessis <mdp0101@gmail.com>
2021-10-19 14:53:33 +0200

    Added functionality to get routers ip, and prevent errors

--- bot.py
+++ bot.py
@@ -1,12 +1,27 @@
-import discord
+from discord.ext import commands
+import variables
+from requests import get
 
-# loading token from enviroment varibale
-TOKEN = 'ODk5OTgxMzYzODkyMDkyOTU4.YW6q9Q.O6SL7U2OUP0t_v_qd4wh6YmTiAI' # token put in better place later
+client = commands.Bot(command_prefix='!')
 
-client = discord.Client()
+@client.command(name='ip')
+async def send_ip(ctx):
+    ip = get('https://api.ipify.org').text
+    await ctx.send(ip + ':25565')
+
+@client.command(name='Hello')
+async def send_kenobi(ctx):
+    if ctx.message.content.lower().replace(' ', '') == '!hellothere':
+        await ctx.send('General Kenobi')
+
+# printing once connected to server
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord')
 
 @client.event
-async def on_ready(): # printing once connected to server
-    print(f'{client.user} has connected to Discord') 
+async def on_command_error(ctx, error):
+    # if isinstance(error, commands.errors.CheckFailure):
+    await ctx.send('Command does not exist')
 
-client.run(TOKEN) # running bot
+client.run(variables.TOKEN) # running bot
